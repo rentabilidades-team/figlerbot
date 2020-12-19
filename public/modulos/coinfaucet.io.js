6daf0b6fa3caeb3b91fff23e696dd248986e4812
Tomás Warynyca <tomas.warynyca@outlook.com>
2020-12-19 21:52:36 +0000

    agregar coinfaucet.io

---
+++ public/modulos/coinfaucet.io.js
@@ -0,0 +1,11 @@
+async function autoclick() {
+	if(window.location.pathname.indexOf("/")>=0){
+        if($("button.main-button.main-button-yellow.login.bg-3").is(':visible')==true){$("button.main-button.main-button-yellow.login.bg-3").click();}
+    }
+    if(window.location.pathname.indexOf("/free")>=0){
+        setInterval(function(){
+            if($("button.main-button-2.roll-button.bg-2").is(':visible')==true){$("button.main-button-2.roll-button.bg-2").click();$("button.main-button-2.roll-button.bg-2").hide();}else{if($("div > div.minutes").is(':visible')==true){$(document).ready(function(){try{window.close();}catch(e){}});}else{location.reload(true);}}
+        },10000);
+    }
+}
+autoclick();
