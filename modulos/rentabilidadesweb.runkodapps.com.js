44b152d982230a8ea7ec7d60df12690402a00db0
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-19 15:29:26 +0000

    Update rentabilidadesweb.runkodapps.com.js

--- modulos/rentabilidadesweb.runkodapps.com.js
+++ modulos/rentabilidadesweb.runkodapps.com.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
     if(window.location.pathname.indexOf('/ultra-bot-plus')>=0){
-        $('input#btnstartstopbot').prop('disabled', false);
-        $('div#alertabot').css("display", "none");
+        document.getElementById('btnstartstopbot').setAttribute("disabled",true);
+        document.getElementById('alertabot').style.display = "none";
     }
     if(window.location.pathname.indexOf('/modulo-digitask')>=0){
         $('input#importardigi').prop('disabled', false);
     }
     if(window.location.pathname.indexOf('/faucetpay')>=0){
-        $('input#importarfaucetpay').prop('disabled', false);
-        $('input#importarfaucetpay').click(function(){
+        document.getElementById('importarfaucetpay').setAttribute("disabled",false);
+        document.getElementById("importarfaucetpay").onclick = function() {
             window.location.href='//faucetpay.io/page/user-admin/deposit?r=368874&importar=1';
-        });
+        };
     }
     if(window.location.pathname.indexOf('/expresscrypto')>=0){
-        $('input#importarexpresscrypto').prop('disabled', false);
-        $('input#importarexpresscrypto').click(function(){
+        document.getElementById('importarexpresscrypto').setAttribute("disabled",false);
+        document.getElementById("importarexpresscrypto").onclick = function() {
             window.location.href='//expresscrypto.io/dashboard?referral=98071&importar=1';
-        });
+        };
     }
     if(window.location.pathname.indexOf('/payeer')>=0){
-        $('input#importarpayeer').prop('disabled', false);
-        $('input#importarpayeer').click(function(){
+        document.getElementById('importarpayeer').setAttribute("disabled",false);
+        document.getElementById("importarexpresscrypto").onclick = function() {
             window.location.href='//payeer.com/en/account/log/?session=402608&importar=1';
-        });
+        };
     }
 });
