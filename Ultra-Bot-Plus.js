689c2e1e52bb18424839396502caa38943cdcf70
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-19 11:25:36 +0000

    ActualizarUltra-Bot-Plus.js, modulos/faucetpay.io.js archivos

---
+++ Ultra-Bot-Plus.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         Ultra Bot Plus
+// @namespace    https://gitlab.com/universales/rentabilidades-team
+// @version      0.2.3
+// @description  Complemento para el navegador del Ultra Bot Plus, aumenta tus ganancias.
+// @author       Rentabilidades Team
+// @updateURL    
+
+// @include      http*://rentabilidadesweb.runkodapps.com/*
+// @include      http*://faucetpay.io/*
+// @include      http*://expresscrypto.io/*
+// @include      http*://payeer.com/en/account/log/*
+// @include      http*://digitask.ru/*
+// @include      http*://freesteam.io/*
+// @include      http*://freenem.com/*
+// @include      http*://freecardano.com/*
+// @include      http*://coinfaucet.io/*
+// @include      http*://freebitcoin.io/*
+// @include      http*://freetether.com/*
+// @include      http*://freeusdcoin.com/*
+// @include      http*://freebinancecoin.com/*
+// @include      http*://freeethereum.com/*
+// @include      http*://free-tron.com/*
+// @include      http*://free-ltc.com/*
+// @include      http*://freedash.io/*
+// @include      http*://freechain.link/*
+// @include      http*://freeneo.io/*
+// @include      http*://bigfreegiveaway.com/*
+// @include      http*://webflex24.com/*
+// @include      http*://rollerbit.net/*
+// @include      http*://freegridco.in/*
+// @include      http*://popspins.com/*
+// @include      http*://randomsatoshi.win/*
+// @include      http*://freebitco.in/*
+// @include      http*://firefaucet.win/*
+// @include      http*://adbtc.top/*
+
+// @include      http*://*.freebcc.org/*
+// @include      http*://dogemate.com/*
+// @include      http*://faupto.com/*
+// @include      http*://*theblogcash.in/*
+// @include      http*://*.dutchycorp.space/*
+// @include      http*://dutchycorp.ovh/*
+// @include      http*://*linkdesh.xyz/*
+// @include      http*://*.100count.net/*
+// @include      http*://100count.net/*
+// @include      http*://zagl.info/*
+
+// @include      http*://goldenmines.biz/*
+// @include      http*://golden-mines.biz/*
+// @include      http*://money-gnomes.pro/*
+// @include      http*://money-gnomes.ru/*
+// @include      http*://cosmo-sfera.biz/*
+// @include      http*://me-farm.me/*
+// @include      http*://wood-man.biz/*
+
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     document.createElement('script').src = '/modulos/'+location.hostname+'.js';
+})();
---
+++ modulos/faucetpay.io.js
@@ -0,0 +1,22 @@
+if(obtenvariable('importar')==1){
+        if(window.location.pathname.indexOf('/page/user-admin/deposit')>=0){
+            sessionStorage.setItem('btc',$('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(1) > div > div.card-body.text-center > input').val().trim());
+            sessionStorage.setItem('doge',$('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(3) > div > div.card-body.text-center > input').val().trim());
+            sessionStorage.setItem('ltc',$('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(4) > div > div.card-body.text-center > input').val().trim());
+            sessionStorage.setItem('bch',$('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(5) > div > div.card-body.text-center > input').val().replace('bitcoincash:', '').trim());
+            sessionStorage.setItem('dash',$('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(6) > div > div.card-body.text-center > input').val().trim());
+            sessionStorage.setItem('dgb',$('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(7) > div > div.card-body.text-center > input').val().trim());
+            sessionStorage.setItem('trx',$('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(8) > div > div.card-body.text-center > input').val().trim());
+            sessionStorage.setItem('usdt',$('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(9) > div > div.card-body.text-center > input').val().trim());
+            location.replace('//'+window.location.hostname+'/account/settings?importar=1');
+        }
+        if(window.location.pathname.indexOf('/account/settings')>=0){
+            location.replace('//'+urlbase+'/faucetpay?fpemail='+$('input[name=email]').val().trim()+'&fpbtc='+sessionStorage.getItem("btc")+'&fpdoge='+sessionStorage.getItem("doge")+'&fpltc='+sessionStorage.getItem("ltc")+'&fpbch='+sessionStorage.getItem("bch")+'&fpdash='+sessionStorage.getItem("dash")+'&fpdgb='+sessionStorage.getItem("dgb")+'&fptrx='+sessionStorage.getItem("trx")+'&fpusdt='+sessionStorage.getItem("usdt"));
+        }
+    }else{
+        if(window.location.pathname.indexOf('/page/user-admin')>=0){
+            if(document.referrer.indexOf(urlbase)>=0){
+                $(document).ready(function(){try{window.close();}catch(e){}});
+            }
+        }
+}
