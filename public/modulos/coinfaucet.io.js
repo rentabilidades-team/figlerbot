66dd0037d25bbf8831990215a0c56d3812520cda
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-20 00:26:24 +0000

    Actualizarpublic/modulos/freeusdcoin.com.js, public/modulos/freetether.com.js, public/modulos/freesteam.io.js, public/modulos/freeneo.io.js, public/modulos/freenem.com.js, public/modulos/freeethereum.com.js, public/modulos/freedash.io.js, public/modulos/freechain.link.js, public/modulos/freecardano.com.js, public/modulos/freebitcoin.io.js, public/modulos/freebinancecoin.com.js, public/modulos/free-tron.com.js, public/modulos/free-ltc.com.js, public/modulos/coinfaucet.io.js archivos

--- public/modulos/coinfaucet.io.js
+++ public/modulos/coinfaucet.io.js
@@ -1,11 +1,19 @@
 async function iniciarBot() {
-	if(window.location.pathname=="/"){
-        if($('input[type=email]:first').val()=='' && $('input[type=password]:first').val()==''){$("button.main-button.main-button-yellow.login.bg-3").click();}
-    }
-    if(window.location.pathname.indexOf("/free")>=0){
-        setInterval(function(){
-            if($("button.main-button-2.roll-button.bg-2").is(':visible')==true){$("button.main-button-2.roll-button.bg-2").click();$("button.main-button-2.roll-button.bg-2").hide();}else{if($("div > div.minutes").is(':visible')==true){$(document).ready(function(){try{window.close();}catch(e){}});}else{location.reload(true);}}
-        },10000);
-    }
+    setInterval(function(){
+        if(window.location.pathname=="/"){
+            if($('input[type=email]:first').val()=='' && $('input[type=password]:first').val()==''){$("button.main-button.main-button-yellow.login.bg-3").click();}
+        }
+        if(window.location.pathname.indexOf("/free")>=0){
+                if($("button.main-button-2.roll-button.bg-2").is(':visible')==true){
+                    $("button.main-button-2.roll-button.bg-2").click();$("button.main-button-2.roll-button.bg-2").hide();
+                }else{
+                    if($("div > div.minutes").is(':visible')==true){
+                        $(document).ready(function(){try{window.close();}catch(e){}});
+                        }else{
+                            location.reload(true);
+                        }
+                }
+        }
+    },10000);
 }
 iniciarBot();
