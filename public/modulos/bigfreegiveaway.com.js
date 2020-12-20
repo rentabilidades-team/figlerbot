5b415720e34f66d7b05fb36889d5dbbf20741f12
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-20 18:45:38 +0000

    Actualizarpublic/modulos/bigfreegiveaway.com.js

--- public/modulos/bigfreegiveaway.com.js
+++ public/modulos/bigfreegiveaway.com.js
@@ -1,5 +1,5 @@
 async function iniciarBot() {
-    import('https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js');
+    import  'https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js';
     if(window.location.pathname.indexOf("/my-tickets")>=0){
         if($('form > button:submit').is(':visible')==true){
             $('form > button:submit').click();
