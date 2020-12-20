d971caa60bea4c6e74aa9470dd430807730d8245
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-20 00:29:40 +0000

    Actualizarpublic/modulos/freebinancecoin.com.js, public/modulos/freebitcoin.io.js, public/modulos/freecardano.com.js, public/modulos/freechain.link.js, public/modulos/freedash.io.js, public/modulos/freeethereum.com.js, public/modulos/freenem.com.js, public/modulos/freeneo.io.js, public/modulos/freesteam.io.js, public/modulos/freetether.com.js, public/modulos/freeusdcoin.com.js, public/modulos/free-tron.com.js, public/modulos/free-ltc.com.js, public/modulos/coinfaucet.io.js archivos

--- public/modulos/freebinancecoin.com.js
+++ public/modulos/freebinancecoin.com.js
@@ -1,7 +1,7 @@
 async function iniciarBot() {
     setInterval(function(){
         if(window.location.pathname=="/"){
-            if($('input[type=email]:first').val()=='' && $('input[type=password]:first').val()==''){$("button.main-button.main-button-yellow.login.bg-3").click();}
+            if($('input[type=email]:first').val()!='' && $('input[type=password]:first').val()!=''){$("button.main-button.main-button-yellow.login.bg-3").click();}
         }
         if(window.location.pathname.indexOf("/free")>=0){
                 if($("button.main-button-2.roll-button.bg-2").is(':visible')==true){
