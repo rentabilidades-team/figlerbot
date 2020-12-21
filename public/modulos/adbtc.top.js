a50338efd691e996343c6b8c92f4daee3a497cc9
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-21 10:13:30 +0000

    Actualizarpublic/modulos/bigfreegiveaway.com.js, public/modulos/adbtc.top.js archivos

--- public/modulos/adbtc.top.js
+++ public/modulos/adbtc.top.js
@@ -1,6 +1,6 @@
 async function iniciarBot() {
     if(window.location.pathname=='/autosurf/session'){
-        if($("body").html().indexOf('target="_blank"')>=0){}else{$(document).ready(function(){try{window.close();}catch(e){}});}
+        if(document.body.innerHTML.search('target="_blank"')<0){$(document).ready(function(){try{window.close();}catch(e){}});}
     }
 }
 iniciarBot();
