e34d7228763b194c3748bbde6c5038d6cb4151a0
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-22 23:35:54 +0000

    Actualizarpublic/modulos/adbtc.top.js, public/Ultra-Bot-Plus.user.js archivos

--- public/modulos/adbtc.top.js
+++ public/modulos/adbtc.top.js
@@ -1,7 +1,8 @@
-export default async function iniciar_modulo() {
+async function iniciar_modulo() {
     if(window.location.pathname=='/autosurf/session'){
         if(document.body.innerHTML.search('target="_blank"')<0){
             cerrar_modulo();
         }
     }
 }
+iniciar_modulo();
