63fbbcbaaac8140c1d5aeeed2fd04e9b91c7f69d
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-25 19:36:31 +0000

    Actualizarpublic/modulos/digitask.ru.js

--- public/modulos/digitask.ru.js
+++ public/modulos/digitask.ru.js
@@ -5,11 +5,15 @@ function importar_libglobal(url,callback) {//Permite importar la libreria global
 importar_libglobal('https://cdn.rawgit.org/universales.gitlab.io/rentabilidades-team/core/libglobal.js',iniciar_modulo);
 async function iniciar_modulo() {
     if(window.location.pathname.indexOf('/notimer_fp')>=0){
-        if(obtener_get('fpdoge')!=null){$('form > div > input[type=text]:nth-child(1)').val(obtenget('fpdoge'));}
-        if(obtener_get('importar')==1){$('form').append( "<input type='hidden' name='guardarurl' value='1'>");}
+        if(obtener_get('fpdoge')!=null){
+            //$('form > div > input[type=text]:nth-child(1)').val(obtenget('fpdoge'));
+        }
+        if(obtener_get('importar')==1){
+            //$('form').append( "<input type='hidden' name='guardarurl' value='1'>");
+        }
     }
     if(window.location.pathname.indexOf('/notimer_fp/faucet.php')>=0){
-        if(libglobal.obtener_get('guardarurl')=='1'){
+        if(obtener_get('guardarurl')=='1'){
             location.replace('//'+dominio_base()+'/modulo-digitask?digitaskurl='+encodeURIComponent(window.location.href));
         }else{
             cerrar_modulo();
