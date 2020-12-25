a71f91499b55549e0053df38a8f7562426549d23
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-25 18:31:03 +0000

    Actualizarpublic/core/libglobal.js

--- public/core/libglobal.js
+++ public/core/libglobal.js
@@ -3,27 +3,33 @@ function dominio_base() {return 'rentabilidadesweb.runkodapps.com';}//Aporta la 
 /*Gestión de tiempo*/
 function espera(ms){//Tiempo de espera await espera(ms);
   if(ms==null){console.log('Error: espera(ms); El valor ms es null');}
-  try{ms=parseInt(ms);}catch(e){console.log(e);}
-  var d = new Date(),d2;
-  do {d2 = new Date();}
-  while(d2-d < ms);
+  else{
+    try{ms=parseInt(ms);}catch(e){console.log(e);}
+    var d = new Date(),d2;
+    do {d2 = new Date();}
+    while(d2-d < ms);
+  }
 }
 
 /*Numero Aleatorio*/
 function numero_aleatorio(min,max) {//Permite obtener un numero aleatorio
   if(min==null || max==null){console.log('Error: numero_aleatorio(min,max); El valor min o max es null.');}
-  try{min=parseInt(min);max=parseInt(max);}catch(e){console.log(e);}
-  return Math.floor((Math.random() * max) + min);
+  else{
+    try{min=parseInt(min);max=parseInt(max);}catch(e){console.log(e);}
+    return Math.floor((Math.random() * max) + min);
+  }
 }
 
 /*Importación de librerias*/
 function importar_libreria(url,callback) {//Permite importar librerias en los módulos
   if(url==null){console.log('Error: importar_libreria(url,callback); El valor  url es null.')}
-  var s = document.createElement("script");s.onload = callback;s.src = url;document.querySelector("head").appendChild(s);
+  else{
+    var s = document.createElement("script");s.onload = callback;s.src = url;document.querySelector("head").appendChild(s);
+  }
 }
 
 /*Realizar click humano*/
-function click(identificador) {//Ejemplo click('button#id-del-boton.class-del-boton.otra-class-del.boton');
+function click(identificador) {//Ejemplo click('button#id-del-boton.class-del-boton.otra-class-del-boton');
   var tiempoespera=numero_aleatorio(1000,5000), x, i;
   x = document.querySelectorAll(identificador);
   if(x.length<0){console.log('Error: click(identificador); No se pudo encontrar el elemento.');}
@@ -34,7 +40,7 @@ function click(identificador) {//Ejemplo click('button#id-del-boton.class-del-bo
       if(x[i].style.visibility=='hidden'){console.log('Click ignorado: El boton está escondido.');}
       if(x[i].disabled==false && x[i].style.visibility=='visible'){
         x[i].click();
-        x[i].setAttribute("type", "hidden");
+        x[i].disabled = true;
       }
     }
   }
@@ -54,14 +60,25 @@ function cerrar_modulo(valor){//Permite cerrar ventanas cuando la página acabe 
 function obtener_get(variable) {var query = window.location.search.substring(1);var vars = query.split("&");for (var i=0; i < vars.length; i++) {var pair = vars[i].split("=");if(pair[0] == variable) {return decodeURIComponent(pair[1]);}}return '';}
 
 /*Gestionar almacenamiento del navegador*/
-function gestionar_datos_del_navegador(accion,dato,valor) {//Accion (0 obtener dato, 1 guardar dato, 2 borrar dato del navegador)
-    if(accion==0){
-        var dato=GM.getValue(dato);
-        if(dato==null){console.log('Error: No se ha obtenido ningun dato.');}else{return dato;}
+function gestionar_datos_del_navegador(accion,dato,valor) {//Accion (0 obtener dato del navegador, 1 guardar dato en el navegador, 2 borrar dato del navegador)
+  if(accion==null){console.log('Error: gestionar_datos_del_navegador(accion,dato,valor); El valor accion es null.');}
+  if(accion==0){
+    var getdato=GM.getValue(dato);
+    if(getdato==null){console.log('Error: gestionar_datos_del_navegador(0,dato,valor); El valor dato es null.');}
+    else{return getdato;}
+  }
+  if(accion==1){
+    if(dato==null || valor==null){console.log('Error: gestionar_datos_del_navegador(1,dato,valor); El valor dato o valor es null.');}
+    else{
+        GM.setValue(dato,valor);  
     }
-    if(accion==1){GM.setValue(dato,valor);}
-    if(accion==2){GM.deleteValue(dato);}
-}
+  }
+  if(accion==2){
+    if(dato==null){console.log('Error: gestionar_datos_del_navegador(2,dato); El valor dato es null.');}
+    else{
+      GM.deleteValue(dato);
+    }
+  }
 
 /*Gestionar cookies*/
 function crear_cookie(cname, cvalue, exdays) {
