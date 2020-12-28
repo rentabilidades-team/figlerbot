113e5f49d7c9ed6c1bc84687b214ca56b54e7cf6
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-28 16:54:17 +0000

    Actualizarpublic/modulos/faucetpay.io.js

--- public/modulos/faucetpay.io.js
+++ public/modulos/faucetpay.io.js
@@ -1,45 +1,36 @@
-/*function importar_libglobal(url,callback) {//Permite importar la libreria global
-    if(url==null){console.log('Error: Es necesario introducir la url de la libreria.');}
-    else{var s = document.createElement("script");s.onload = callback;s.src = url;document.querySelector("head").appendChild(s);}
-}
-importar_libglobal('https://cors-anywhere.herokuapp.com/universales.gitlab.io/rentabilidades-team/core/libglobal.js',iniciar_modulo);*/
-
 import * as libglobal from "https://cors-anywhere.herokuapp.com/universales.gitlab.io/rentabilidades-team/core/libglobal.js";
 
-console.log(libglobal.dominio_base());
-/*
 async function iniciar_modulo() {
-    if(obtener_get('importar')==1){
+    if(libglobal.obtener_get('importar')==1){
         if(window.location.pathname.indexOf('/page/user-admin/deposit')>=0){
             var temp=null;
-            temp=obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(1) > div > div.card-body.text-center > input','val');
+            temp=libglobal.obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(1) > div > div.card-body.text-center > input','val');
             sessionStorage.setItem('btc',temp);
-            temp=obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(3) > div > div.card-body.text-center > input','val');
+            temp=libglobal.obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(3) > div > div.card-body.text-center > input','val');
             sessionStorage.setItem('doge',temp);
-            temp=obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(4) > div > div.card-body.text-center > input','val');
+            temp=libglobal.obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(4) > div > div.card-body.text-center > input','val');
             sessionStorage.setItem('ltc',temp);
-            temp=obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(5) > div > div.card-body.text-center > input','val');
+            temp=libglobal.obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(5) > div > div.card-body.text-center > input','val');
             sessionStorage.setItem('bch',temp);
-            temp=obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(6) > div > div.card-body.text-center > input','val');
+            temp=libglobal.obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(6) > div > div.card-body.text-center > input','val');
             sessionStorage.setItem('dash',temp);
-            temp=obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(7) > div > div.card-body.text-center > input','val');
+            temp=libglobal.obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(7) > div > div.card-body.text-center > input','val');
             sessionStorage.setItem('dgb',temp);
-            temp=obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(8) > div > div.card-body.text-center > input','val')
+            temp=libglobal.obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(8) > div > div.card-body.text-center > input','val')
             sessionStorage.setItem('trx',temp);
-            temp=obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(9) > div > div.card-body.text-center > input','val');
+            temp=libglobal.obtener('body > div.content.content-fixed > div > div.row.row-xs > div > div.row > div:nth-child(9) > div > div.card-body.text-center > input','val');
             sessionStorage.setItem('usdt',temp);
             location.replace('//'+window.location.hostname+'/account/settings?importar=1');
         }
         if(window.location.pathname.indexOf('/account/settings')>=0){
-            location.replace('//'+dominio_base()+'/faucetpay?fpemail='+obtener('input[name=email]','val')+'&fpbtc='+sessionStorage.getItem("btc")+'&fpdoge='+sessionStorage.getItem("doge")+'&fpltc='+sessionStorage.getItem("ltc")+'&fpbch='+sessionStorage.getItem("bch")+'&fpdash='+sessionStorage.getItem("dash")+'&fpdgb='+sessionStorage.getItem("dgb")+'&fptrx='+sessionStorage.getItem("trx")+'&fpusdt='+sessionStorage.getItem("usdt"));
+            location.replace('//'+libglobal.dominio_base()+'/faucetpay?fpemail='+libglobal.obtener('input[name=email]','val')+'&fpbtc='+sessionStorage.getItem("btc")+'&fpdoge='+sessionStorage.getItem("doge")+'&fpltc='+sessionStorage.getItem("ltc")+'&fpbch='+sessionStorage.getItem("bch")+'&fpdash='+sessionStorage.getItem("dash")+'&fpdgb='+sessionStorage.getItem("dgb")+'&fptrx='+sessionStorage.getItem("trx")+'&fpusdt='+sessionStorage.getItem("usdt"));
         }
     }else{
         if(window.location.pathname.indexOf('/page/user-admin')>=0){
             if(document.referrer.indexOf(dominio_base())>=0){
-                cerrar_modulo();
+                libglobal.cerrar_modulo();
             }
         }
     }
 }
 iniciar_modulo();
-*/
