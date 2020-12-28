88cc9a83fc2ea3f08e14321e5ee6c7789cd94ff7
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-28 18:28:30 +0000

    Actualizarpublic/modulos/freeusdcoin.com.js, public/modulos/freetether.com.js, public/modulos/freesteam.io.js, public/modulos/freeneo.io.js, public/modulos/freenem.com.js, public/modulos/freeethereum.com.js, public/modulos/freedash.io.js, public/modulos/freechain.link.js, public/modulos/freecardano.com.js, public/modulos/freebitcoin.io.js, public/modulos/freebinancecoin.com.js, public/modulos/free-tron.com.js, public/modulos/free-ltc.com.js, public/modulos/coinfaucet.io.js archivos

--- public/modulos/freesteam.io.js
+++ public/modulos/freesteam.io.js
@@ -1,3 +1,3 @@
 import * as freefamily from "https://cors-anywhere.herokuapp.com/universales.gitlab.io/rentabilidades-team/modulos/templates/freefamily.js";
 
-freefamily.iniciarBot();
+freefamily.template();
