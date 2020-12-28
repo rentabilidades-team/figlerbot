66d9c5fbc31a8b02ae13409ea0a6b37d75d65d07
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-28 17:53:19 +0000

    Actualizarpublic/modulos/payeer.com.js

--- public/modulos/payeer.com.js
+++ public/modulos/payeer.com.js
@@ -3,7 +3,7 @@ import * as libglobal from "https://cors-anywhere.herokuapp.com/universales.gitl
 async function iniciar_modulo() {
     if(libglobal.obtener_get('importar')==1){
         if(window.location.pathname.indexOf('/en/account/log/')>=0){
-            location.replace('//'+libglobal.dominio_base()+'/payeer?payeerid='+libglobal.obtener('#btn-account > b').replace(/<[^>]*>?/gm, ''));//Revisar
+            location.replace('//'+libglobal.dominio_base()+'/payeer?payeerid='+libglobal.obtener('#btn-account > b').replace(/<[^>]*>?/gm, '').trim());//Revisar
         }
     }
 }
