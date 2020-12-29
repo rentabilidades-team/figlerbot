89ee2dcaac76e5c0d706a3c1cada340fb717f154
Tomás Warynyca <tomaswarynyca@hyndax.com.ar>
2020-12-29 19:42:15 +0000

    Update freefamily.js

--- public/modulos/templates/freefamily.js
+++ public/modulos/templates/freefamily.js
@@ -13,7 +13,7 @@ async function template() {
     /*AutoClaim*/
     if (window.location.pathname.indexOf("/free") >= 0) {
         if (libglobal.click("button.main-button-2.roll-button.bg-2")) {
-            libglobal.espera(5000);
+            while (!$("div > div.minutes").is(':visible')) {}
         }
 
         if (libglobal.obtener_cookie("free-roll") < 8) {
