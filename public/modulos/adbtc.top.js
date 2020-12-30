7fb4fef1083057d68b53dc8bd092ba3438336788
universales <3558634-universales@users.noreply.gitlab.com>
2020-12-30 22:35:24 +0000

    Actualizarpublic/modulos/adbtc.top.js

--- public/modulos/adbtc.top.js
+++ public/modulos/adbtc.top.js
@@ -1,7 +1,6 @@
 import {espera_carga, cerrar_modulo} from "../core/libglobal.js";
 
 async function iniciar_modulo() {
-    console.log('Entré??????????????');
     if(window.location.pathname=='/autosurf/session'){
         if(document.body.innerHTML.search('target="_blank"')<0){
             cerrar_modulo();
@@ -9,4 +8,3 @@ async function iniciar_modulo() {
     }
 }
 espera_carga(iniciar_modulo);
-console.log('Entré');
