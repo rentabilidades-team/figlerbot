6c4c8f14e0bf20864c33ed18fbbb193539f373c6
Tomás Warynyca <tomas.warynyca@outlook.com>
2021-02-01 19:29:27 -0300

    fix: renaming the variable espera to wait

--- core/resources/unstable/human-simulator/click.js
+++ core/resources/unstable/human-simulator/click.js
@@ -1,34 +1,48 @@
-import {random_numbers} from "../random/number.js";
+import { random_numbers } from "../random/number.js";
 
 function click_human(identificador) {
-  var tiempoespera=random_numbers(1000,5000), x, i, style, visibility, display;
+  var tiempoespera = random_numbers(1000, 5000),
+    x,
+    i,
+    style,
+    visibility,
+    display;
   x = document.querySelectorAll(identificador);
-  if(x.length==0){console.log('Click ignorado: click(identificador); No se pudo encontrar el elemento '+identificador+' .');}
-  else{
-    i=0;
+  if (x.length == 0) {
+    console.log(
+      "Click ignorado: click(identificador); No se pudo encontrar el elemento " +
+        identificador +
+        " ."
+    );
+  } else {
+    i = 0;
     /* Comprobación nodo padre */
     style = window.getComputedStyle(x[i].parentNode);
-    visibility = style.getPropertyValue('visibility').toLowerCase();
-    display = style.getPropertyValue('display').toLowerCase();
-    if(visibility=='hidden' || display=='none'){x[i].style.display='none';}
+    visibility = style.getPropertyValue("visibility").toLowerCase();
+    display = style.getPropertyValue("display").toLowerCase();
+    if (visibility == "hidden" || display == "none") {
+      x[i].style.display = "none";
+    }
     /* Comprobación nodo hijo */
-    if(x[i].disabled==true){
-      console.log('Click ignorado: El boton está deshabilidado.');
+    if (x[i].disabled == true) {
+      console.log("Click ignorado: El boton está deshabilidado.");
     }
     style = window.getComputedStyle(x[i]);
-    visibility = style.getPropertyValue('visibility').toLowerCase();
-    display = style.getPropertyValue('display').toLowerCase();
-    if(visibility=='hidden' || display=='none'){
-      console.log('Click ignorado: El boton está escondido.');
+    visibility = style.getPropertyValue("visibility").toLowerCase();
+    display = style.getPropertyValue("display").toLowerCase();
+    if (visibility == "hidden" || display == "none") {
+      console.log("Click ignorado: El boton está escondido.");
     }
     /* Acción click */
-    espera(tiempoespera);
-    if(x[i].disabled==false && visibility!='hidden' && display!='none'){
+    wait(tiempoespera);
+    if (x[i].disabled == false && visibility != "hidden" && display != "none") {
       x[i].click();
       x[i].disabled = true;
       return true;
-    }else{return false;}
+    } else {
+      return false;
+    }
   }
 }
 
-export {click_human};
+export { click_human };
