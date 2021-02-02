616f360b9a17aff935a959b4b4dd1dd384ac75f8
wuniversales <48658161+wuniversales@users.noreply.github.com>
2021-02-02 12:26:05 +0100

    fix: Bug fix

--- modules/stable/bigfreegiveaway.com.js
+++ modules/stable/bigfreegiveaway.com.js
@@ -1,14 +1,14 @@
 import {
   load_wait,
   click,
-  espera,
+  wait,
   close_module,
 } from "../../core/resources/stable/libglobal.js";
 
 async function iniciar_modulo() {
   if (window.location.pathname.indexOf("/my-tickets") >= 0) {
     click("form > button");
-    espera(5000);
+    await wait(5000);
     close_module();
   }
 }
